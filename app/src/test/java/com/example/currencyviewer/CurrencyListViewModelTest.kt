bf08c19e58f34441bfbe5471fea3b855d1ff454c
Gadson, Osiris <osiris.gadson@delta.com>
2023-09-06 21:17:36 -0400

    remove redundant test file and add initial unit test cases

---
+++ app/src/test/java/com/example/currencyviewer/CurrencyListViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.example.currencyviewer
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class CurrencyListViewModelTest {
+    @Test
+    fun `rate service should return list of rates when api response is successful`() {
+        //TODO make this pass when implementation is done
+        fail()
+    }
+
+    @Test
+    fun `rate service should return an empty list when api response is unsuccessful`() {
+        //TODO make this pass when implementation is done
+        fail()
+    }
+}
