335b73576b44e3ae95ce99fa7e0becb1df5333c6
Gadson, Osiris <osiris.gadson@delta.com>
2023-09-07 10:52:47 -0400

    add divider between list elements

--- app/src/main/java/com/example/currencyviewer/application/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyviewer/application/ui/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -115,6 +117,7 @@ fun CurrencyListView(
                         }) {
                         CurrencyListItem(rate = it)
                     }
+                    Divider(color = Color.Black)
                 }
             }
         }
